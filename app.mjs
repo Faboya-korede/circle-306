124320a9691089b896f957dad7764ec8f993e263
Gideon Anyalewechi <43853304+getgiddy@users.noreply.github.com>
2022-08-17 13:41:03 +0000

    add basic functionality

--- app.mjs
+++ app.mjs
@@ -1,17 +1,40 @@
+function redact(textBody, wordsToRedact, scrambleChar) {
+  for (const word of wordsToRedact) {
+    textBody = textBody.replaceAll(word, scrambleChar.repeat(word.length));
+  }
+  return textBody;
+}
+
+function getMainText() {
+  return document.getElementById("textBody").value;
+}
+
+function getWordsToRedact() {
+  // should return array
+  const csWords = document.getElementById("textToRedact").value;
+  return csWords.split(",").map((text) => text.trim());
+}
+
+function getScrambleChar() {
+  return document.getElementById("scrambleChar").value;
+}
+
+function setDisplay(redactedText) {
+  // do something with text body
+  let display = document.querySelector(".redactr-display");
+  if (display) {
+    display.innerHTML = redactedText;
+  }
+}
+
 function startApp() {
-    // Your entire app should not necessarily be coded inside this 
-    // single function (though there's no penalty for that), 
-    // so create and use/call additional functions from here
-  
-    // pls remove the below and make some magic in here!
-    console.log('make magic in here!');
-  
-    const header = document.querySelector('h2');
-    if(header) {
-      header.textContent = 'make some magic here!!';
-    }
-  };
-  
-  // ======= DO NOT EDIT ============== //
-  export default startApp;
-  // ======= EEND DO NOT EDIT ========= //
+  document.getElementById("submitButton").addEventListener("click", (event) => {
+    event.preventDefault();
+    const redactedTextBody = redact(getMainText(), getWordsToRedact(), getScrambleChar());
+    setDisplay(redactedTextBody);
+  });
+}
+
+// ======= DO NOT EDIT ============== //
+export default startApp;
+// ======= EEND DO NOT EDIT ========= //
